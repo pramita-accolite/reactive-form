79746ca229bab13741a731bbc9c11940484bf892
pramita-accolite <pramita.bhattacherjee@accolitedigital.com>
2022-12-06 10:24:14 +0530

    changed it to reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './modules/layout/layout.module';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,8 +11,11 @@ import { LayoutModule } from './modules/layout/layout.module';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule,
     LayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,ReactiveFormsModule,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-active-vendor',
+
   templateUrl: './active-vendor.component.html',
   styleUrls: ['./active-vendor.component.scss']
 })
+
 export class ActiveVendorComponent implements OnInit {
 
   constructor() { }
+loginForm = new FormGroup ({
+  name : new FormControl('',[Validators.required]),
+  email : new FormControl('',[Validators.required]),
+  bio : new FormControl('',[Validators.required]),
+  
+})
+  
+myVal!:any
+
+loginUser()
+{
+   this.myVal = this.loginForm.value;
+this.loginForm.reset();
+  
+}
+
+get name()
+{
+  return this.loginForm.get('name')
+}
+
+get email()
+{
+  return this.loginForm.get('email')
+}
+get bio()
+{
+  return this.loginForm.get('bio')
+}
 
   ngOnInit(): void {
   }
+
+  
+ 
 
 }
