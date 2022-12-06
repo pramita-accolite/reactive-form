0806f7d9a3a6cdd717b3bb3dc1bd81b14b429995
pramita-accolite <pramita.bhattacherjee@accolitedigital.com>
2022-12-06 10:32:57 +0530

    added proper validators

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,50 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,ReactiveFormsModule,FormGroup, Validators } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-active-vendor',
-
   templateUrl: './active-vendor.component.html',
   styleUrls: ['./active-vendor.component.scss']
 })
-
 export class ActiveVendorComponent implements OnInit {
-
+  ngOnInit(): void {
+  }
   constructor() { }
-loginForm = new FormGroup ({
-  name : new FormControl('',[Validators.required]),
-  email : new FormControl('',[Validators.required]),
-  bio : new FormControl('',[Validators.required]),
-  
-})
-  
-myVal!:any
-
-loginUser()
-{
-   this.myVal = this.loginForm.value;
-this.loginForm.reset();
-  
-}
-
+  profileForm = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',{validators: [Validators.required, Validators.email], updateOn: 'blur'}),
+    bio: new FormControl('',[Validators.required]),
+    domain: new FormControl('',[Validators.required]),
+  });
+  data:any;
+  onSubmit() {
+    this.data = this.profileForm.value;
+    this.profileForm.reset();
+  }
 get name()
 {
-  return this.loginForm.get('name')
+  return this.profileForm.get('name')
 }
-
 get email()
 {
-  return this.loginForm.get('email')
+  return this.profileForm.get('email')
 }
 get bio()
 {
-  return this.loginForm.get('bio')
+  return this.profileForm.get('bio')
 }
-
-  ngOnInit(): void {
-  }
-
-  
- 
-
+get domain()
+{
+  return this.profileForm.get('domain')
+}
 }
